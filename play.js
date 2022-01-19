9a9a3293241b9892d8a8af705fd1bcbdabcd190d
Edwin Ly <edwinjly@gmail.com>
2022-01-19 14:58:54 -0500

    client setup complete

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541,
+  });
+
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    conn.write("Hello from client!");
+  });
+
+  conn.on("data", (data) => {
+    console.log(`"Server says: ", ${data}`);
+  });
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
