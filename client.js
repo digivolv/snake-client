66d63d79dfd7d06f84e5dbc08325fab660fcb757
Edwin Ly <edwinjly@gmail.com>
2022-01-19 16:16:34 -0500

    play.js implemented

--- client.js
+++ client.js
@@ -14,7 +14,7 @@ const connect = function () {
   });
 
   conn.on("connect", () => {
-    conn.write("Name: Ed");
+    conn.write("Name: Edw");
     console.log("hi");
   });
 
@@ -25,30 +25,3 @@ console.log("Connecting ...");
 connect();
 
 module.exports = { connect };
-
-// const net = require("net");
-// const connect = function () {
-//   const conn = net.createConnection({
-//     host: "165.227.47.243",
-//     port: 50541,
-//   });
-
-//   conn.on("data", (data) => {
-//     console.log(data.toString());
-//     conn.end();
-//   });
-
-//   conn.on("connect", () => {
-//     conn.write("Name: jae");
-//     console.log("hi");
-//   });
-//   // interpret incoming data as text
-//   conn.setEncoding("utf8");
-
-//   return conn;
-// };
-
-// console.log("Connecting ...");
-// connect();
-
-// module.exports = connect;
--- play.js
+++ play.js
@@ -1,30 +1,50 @@
 const net = require("net");
-const { connect } = require("client");
+const { connect } = require("./client");
+const { stdin } = require("process");
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: "165.227.47.243",
-    port: 50541,
-  });
-
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    conn.write("Name: Ed");
-    conn.end();
-  });
-
-  // conn.on("connect", () => {
-  //   console.log("Hello from client!");
-  //   conn.end();
-  // });
-
-  conn.on("data", (data) => {
-    console.log(`"Server says: ", ${data}`);
-  });
-
-  return conn;
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
 };
+setupInput();
 
-console.log("Connecting ...");
-connect();
+const handleUserInput =
+  ("keypress",
+  (key) => {
+    if (key === "\u0003") {
+      process.exit();
+    }
+  });
+
+stdin.on("data", handleUserInput);
+
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "165.227.47.243",
+//     port: 50541,
+//   });
+
+//   conn.setEncoding("utf8");
+
+//   conn.on("connect", () => {
+//     conn.write("Name: Ed");
+//     conn.end();
+//   });
+
+//   // conn.on("connect", () => {
+//   //   console.log("Hello from client!");
+//   //   conn.end();
+//   // });
+
+//   conn.on("data", (data) => {
+//     console.log(`"Server says: ", ${data}`);
+//   });
+
+//   return conn;
+// };
+
+// console.log("Connecting ...");
+// connect();
